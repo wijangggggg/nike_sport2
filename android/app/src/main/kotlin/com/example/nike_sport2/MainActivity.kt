d0317a857cead5cd0230e9c928f172306f6c136d
wijangggggg <wijangsriyadi@gmail.com>
2025-06-19 11:41:14 +0700

    membuat file flutter

---
+++ android/app/src/main/kotlin/com/example/nike_sport2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nike_sport2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/start.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Startpage extends StatefulWidget {
+  const Startpage({super.key});
+
+  @override
+  State<Startpage> createState() => _StartpageState();
+}
+
+class _StartpageState extends State<Startpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
